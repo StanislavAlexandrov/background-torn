f3063dccc37c666e37c2a6c88181e910098aea45
Stanislav Alexandrov <stanislavalexandrov@gmail.com>
2026-07-11 09:54:01 -0500

    Bust cached version switch assets

--- js/index.js
+++ js/index.js
@@ -3,11 +3,11 @@ const buttons = document.querySelectorAll('[data-version]');
 
 const versions = {
     old: {
-        source: './versions/old/index.html',
+        source: './versions/old/index.html?v=2',
         title: 'Old background-torn version',
     },
     new: {
-        source: './versions/new/index.html',
+        source: './versions/new/index.html?v=2',
         title: 'New background-torn version',
     },
 };
